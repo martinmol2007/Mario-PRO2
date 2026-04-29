be78a0055aa1730d4f44df2898c7b49e79da0258
Martin <martinmolinapavon1970@gmail.com>
2026-04-29 17:55:10 +0200

    Cambios

--- game.cc
+++ game.cc
@@ -59,7 +59,7 @@ void Game::update_objects(pro2::Window& window) {
                 mario_.poner_animacion();
                 it = monedas_.erase(it);
                 // cout << "CONTADOR MONEDAS: " << contador_monedas_ << endl;
-                // cout << "TAMAÑO DE LA LISTA DE MONEDAS: " << monedas_.size() << endl;
+                cout << "TAMAÑO DE LA LISTA DE MONEDAS: " << monedas_.size() << endl;
             }
             // No borras
             else {
@@ -73,7 +73,7 @@ void Game::update_objects(pro2::Window& window) {
 
 
 void Game::update_camera(pro2::Window& window) {
-    
+
     // Movimiento de camara mas fluido
 
     const Pt pos = mario_.pos();
