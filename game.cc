d6e1d7e7b7f475ff7ed3599bda3879cd2740d9ec
Martin <martinmolinapavon1970@gmail.com>
2026-04-29 16:31:50 +0200

    Cambios

--- game.cc
+++ game.cc
@@ -59,6 +59,7 @@ void Game::update_objects(pro2::Window& window) {
                 mario_.poner_animacion();
                 it = monedas_.erase(it);
                 // cout << "CONTADOR MONEDAS: " << contador_monedas_ << endl;
+                // cout << "TAMAÑO DEL VECTOR DE MONEDAS: " << monedas_.size() << endl;
             }
             // No borras
             else {
